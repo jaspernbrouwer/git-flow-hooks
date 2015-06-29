bb52f33659de1c404f71c0cb60a0c6653b8ce156
Michael Delle <dravenlon@gmail.com>
2015-06-28 21:51:44 -0400

    Adding ROOT_DIR variable

--- modules/gitlog-to-changelog.sh
+++ modules/gitlog-to-changelog.sh
@@ -13,23 +13,25 @@ if [ $# -gt 2 ]; then
     __print_usage
 fi
 
+ROOT_DIR=$(git rev-parse --show-toplevel 2> /dev/null)
 VERSION="$1"
-CHANGELOG_FILE="$2"
+CHANGELOG_FILE="$ROOT_DIR/$2"
 
 if [ -f $CHANGELOG_FILE ]; then
-    echo "Version $VERSION:" > tmpfile
-    git log --no-merges --pretty=format:" * %s (%an)" "v$VERSION"...HEAD >> tmpfile
-    echo "" >> tmpfile
-    echo "" >> tmpfile
-    cat $CHANGELOG_FILE >> tmpfile
+    TEMP_FILE="$ROOT_DIR/tmpfile"
+    echo "Version $VERSION:" > $TEMP_FILE
+    git log --no-merges --pretty=format:" * %s (%an)" "v$VERSION"...HEAD >> $TEMP_FILE
+    echo "" >> $TEMP_FILE
+    echo "" >> $TEMP_FILE
+    cat $CHANGELOG_FILE >> $TEMP_FILE
     mv tmpfile $CHANGELOG_FILE
-    git add $CHANGELOG_FILE
-    git commit -m "Wrote changes $CHANGELOG_FILE"
+    #git add $CHANGELOG_FILE
+    #git commit -m "Wrote changes $CHANGELOG_FILE"
 else
-    echo "Version 0.1.0" > $CHANGELOG_FILE
-    git log --pretty=format:" - %s (%an)" >> $CHANGELOG_FILE
+    echo "Version $VERSION:" > $CHANGELOG_FILE
+    git log --pretty=format:" * %s (%an)" >> $CHANGELOG_FILE
     echo "" >> $CHANGELOG_FILE
     echo "" >> $CHANGELOG_FILE
-    git add $CHANGELOG_FILE
-    git commit -m "Added CHANGELOG file"
+    #git add $CHANGELOG_FILE
+    #git commit -m "Added CHANGELOG file"
 fi
