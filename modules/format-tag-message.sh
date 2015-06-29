f88ad8a5b85a4f34a76ada935ab171bcc40e9fe1
Michael Delle <dravenlon@gmail.com>
2015-06-29 02:35:52 -0400

    Update format-tag-message.sh

--- modules/format-tag-message.sh
+++ modules/format-tag-message.sh
@@ -3,7 +3,7 @@
 # Move to own file.
 USE_CHANGELOG=$(__get_use_changelog)
 if [ "$USE_CHANGELOG" = true ]; then
-	VERSION_CURRENT=$(__get_current_version)
+    VERSION_CURRENT=$(__get_current_version)
     CHANGELOG_FILE_NAME=$(__get_changelog_file_name)
     CHANGE=$($HOOKS_DIR/modules/gitlog-to-changelog.sh $VERSION_CURRENT $VERSION "$CHANGELOG_FILE_NAME")
 fi
